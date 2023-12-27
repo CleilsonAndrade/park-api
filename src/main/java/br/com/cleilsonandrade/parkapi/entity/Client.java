ad53c8a7a789cde5b5e39913f3566f7bca9b3866
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-27 00:49:38 -0300

    ✨ feat: entity Client

---
+++ src/main/java/br/com/cleilsonandrade/parkapi/entity/Client.java
@@ -0,0 +1,89 @@
+package br.com.cleilsonandrade.parkapi.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_client")
+@EntityListeners(AuditingEntityListener.class)
+public class Client implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 100)
+  private String name;
+
+  @Column(name = "cpf", nullable = false, unique = true, length = 11)
+  private String cpf;
+
+  @OneToOne
+  @JoinColumn(name = "id_user", nullable = false)
+  private User user;
+
+  @CreatedBy
+  @Column(name = "createdBy")
+  private String createdBy;
+
+  @LastModifiedBy
+  @Column(name = "modifiedBy")
+  private String modifiedBy;
+
+  @CreatedDate
+  @Column(name = "createdAt")
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updatedAt")
+  private LocalDateTime updatedAt;
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Client other = (Client) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
