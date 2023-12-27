ac6d29a6a9f8db0fa0bb17cb2ef6c3cafa222909
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-26 23:39:51 -0300

    📑 docs: authentication resource

--- src/main/java/br/com/cleilsonandrade/parkapi/web/controller/AuthenticationController.java
+++ src/main/java/br/com/cleilsonandrade/parkapi/web/controller/AuthenticationController.java
@@ -13,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.cleilsonandrade.parkapi.jwt.JwtToken;
 import br.com.cleilsonandrade.parkapi.jwt.JwtUserDetailsService;
 import br.com.cleilsonandrade.parkapi.web.dto.UserLoginDTO;
+import br.com.cleilsonandrade.parkapi.web.dto.UserResponseDTO;
 import br.com.cleilsonandrade.parkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.validation.Valid;
@@ -29,6 +34,11 @@ public class AuthenticationController {
   private final JwtUserDetailsService detailsService;
   private final AuthenticationManager authenticationManager;
 
+  @Operation(summary = "Authenticate to API", description = "Authentication feature", responses = {
+      @ApiResponse(responseCode = "200", description = "Authentication carried out successfully and return 'Bearer token'", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+      @ApiResponse(responseCode = "400", description = "Disabled by credentials", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+      @ApiResponse(responseCode = "422", description = "Invalid field(s)", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+  })
   @PostMapping
   public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDTO dto, HttpServlet request) {
     log.info("Login authentication process {}", dto.getUsername());
