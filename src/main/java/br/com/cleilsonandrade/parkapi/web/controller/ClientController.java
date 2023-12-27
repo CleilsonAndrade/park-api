d97f81929f19f5abe1a6069dfdd2ec773cd93c2b
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-27 18:10:25 -0300

    📑 docs: client resource get all clients

--- src/main/java/br/com/cleilsonandrade/parkapi/web/controller/ClientController.java
+++ src/main/java/br/com/cleilsonandrade/parkapi/web/controller/ClientController.java
@@ -2,6 +2,7 @@ package br.com.cleilsonandrade.parkapi.web.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,14 +21,17 @@ import br.com.cleilsonandrade.parkapi.service.UserService;
 import br.com.cleilsonandrade.parkapi.web.dto.ClientCreateDTO;
 import br.com.cleilsonandrade.parkapi.web.dto.ClientPageableDTO;
 import br.com.cleilsonandrade.parkapi.web.dto.ClientResponseDTO;
-import br.com.cleilsonandrade.parkapi.web.dto.UserResponseDTO;
 import br.com.cleilsonandrade.parkapi.web.dto.mapper.ClientMapper;
 import br.com.cleilsonandrade.parkapi.web.dto.mapper.ClientPageableMapper;
 import br.com.cleilsonandrade.parkapi.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -43,8 +47,8 @@ public class ClientController {
 
   @Operation(summary = "Create a new client", description = "Feature to create a new client, linked to an already registered user"
       +
-      "Request requires use of a 'Bearer token'. Restricted access toRole='CLIENT'", responses = {
-          @ApiResponse(responseCode = "201", description = "Resource created successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+      "Request requires use of a 'Bearer token'. Restricted access to Role='CLIENT'", security = @SecurityRequirement(name = "security"), responses = {
+          @ApiResponse(responseCode = "201", description = "Resource created successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientResponseDTO.class))),
           @ApiResponse(responseCode = "403", description = "Feature not allowed for profile ADMIN", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
           @ApiResponse(responseCode = "409", description = "CPF client is already registered in the system", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
           @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
@@ -61,9 +65,9 @@ public class ClientController {
 
   @Operation(summary = "Localize client", description = "Feature to localize client, get by id"
       +
-      "Request requires use of a 'Bearer token'. Restricted access toRole='ADMIN'", responses = {
-          @ApiResponse(responseCode = "200", description = "Resource localized successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
-          @ApiResponse(responseCode = "403", description = "Feature not allowed for profileCLIENT", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+      "Request requires use of a 'Bearer token'. Restricted access to Role='ADMIN'", security = @SecurityRequirement(name = "security"), responses = {
+          @ApiResponse(responseCode = "200", description = "Resource localized successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientResponseDTO.class))),
+          @ApiResponse(responseCode = "403", description = "Feature not allowed for profile CLIENT", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
           @ApiResponse(responseCode = "404", description = "Client not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
       })
   @GetMapping("/{id}")
@@ -73,9 +77,19 @@ public class ClientController {
     return ResponseEntity.ok(ClientMapper.toDto(client));
   }
 
+  @Operation(summary = "Retrieve clients list", description = "Request requires use of 'Bearer token'. Restricted access to Role='ADMIN'", security = @SecurityRequirement(name = "security"), parameters = {
+      @Parameter(in = ParameterIn.QUERY, name = "page", content = @Content(schema = @Schema(type = "integer", defaultValue = "0")), description = "Represents page returned"),
+      @Parameter(in = ParameterIn.QUERY, name = "size", content = @Content(schema = @Schema(type = "integer", defaultValue = "20")), description = "Represents the total number of elements per page"),
+      @Parameter(in = ParameterIn.QUERY, name = "sort", hidden = true, array = @ArraySchema(schema = @Schema(type = "string", defaultValue = "id,asc")), description = "Represents the ordering of results. Accepts multiple sorting criteria are supported"),
+  }, responses = {
+      @ApiResponse(responseCode = "200", description = "Resource localized successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClientResponseDTO.class))),
+      @ApiResponse(responseCode = "403", description = "Feature not allowed for profile CLIENT", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+      @ApiResponse(responseCode = "404", description = "Client not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+  })
   @GetMapping()
   @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<ClientPageableDTO> getAll(Pageable pageable) {
+  public ResponseEntity<ClientPageableDTO> getAll(
+      @Parameter(hidden = true) @PageableDefault(size = 5, sort = { "name" }) Pageable pageable) {
     Page<ClientProjection> clients = clientService.searchAll(pageable);
     return ResponseEntity.ok(ClientPageableMapper.toDto(clients));
   }
