1714a30a6829a916cc9c7fc50b2d58cbb2155027
CleilsonAndrade <cleilsonjose@hotmail.com>
2023-12-28 14:59:53 -0300

    ✨ feat: parking method find by code in service

--- src/main/java/br/com/cleilsonandrade/parkapi/repository/ParkingRepository.java
+++ src/main/java/br/com/cleilsonandrade/parkapi/repository/ParkingRepository.java
@@ -1,9 +1,13 @@
 package br.com.cleilsonandrade.parkapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.cleilsonandrade.parkapi.entity.Parking;
 
 public interface ParkingRepository extends JpaRepository<Parking, Long> {
+
+  Optional<Parking> findByCode(String code);
 
 }
--- src/main/java/br/com/cleilsonandrade/parkapi/service/ParkingService.java
+++ src/main/java/br/com/cleilsonandrade/parkapi/service/ParkingService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.cleilsonandrade.parkapi.entity.Parking;
 import br.com.cleilsonandrade.parkapi.exception.CodeUniqueViolationException;
+import br.com.cleilsonandrade.parkapi.exception.EntityNotFoundException;
 import br.com.cleilsonandrade.parkapi.repository.ParkingRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +22,11 @@ public class ParkingService {
     } catch (DataIntegrityViolationException e) {
       throw new CodeUniqueViolationException(String.format("Parking with code '%s' already exists", parking.getCode()));
     }
+  }
+
+  @Transactional(readOnly = true)
+  public Parking findByCode(String code) {
+    return parkingRepository.findByCode(code).orElseThrow(
+        () -> new EntityNotFoundException(String.format("Parking with code '%s' not found", code)));
   }
 }
